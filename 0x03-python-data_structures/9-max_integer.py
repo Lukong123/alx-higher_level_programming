3ccb7dc62a7a40965db97ec9d15ae26550c12936
Anne Machinda <annemachinda@gmail.com>
2021-09-13 19:28:39 +0100

    new

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if len(my_list) < 1:
         return None
-    else:
-        largest = 0
-        for i in range(0, len(my_list)):
-            if my_list[i] > largest:
-                largest = my_list[i]
-        return largest
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return list_copy[-1]
