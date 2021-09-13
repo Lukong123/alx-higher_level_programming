b5a45373330c9c82620ce39478069df5cac6f289
Anne Machinda <annemachinda@gmail.com>
2021-09-13 14:24:38 +0100

    adjusted for None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    return "Length: {:d} - First character: {}".format(length, first)
+    if len(sentence) == 0:
+        return None
+    else:
+        length = len(sentence)
+        first = sentence[0]
+        return "Length: {:d} - First character: {}".format(length, first)
