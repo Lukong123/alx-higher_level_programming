bf99c970086fad562d60d0f7510ed144dddad42b
Anne Machinda <annemachinda@gmail.com>
2021-10-13 17:07:33 +0100

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A module for json representation
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ A function that returns json representation of an object"""
+    return json.dumps(my_obj)
