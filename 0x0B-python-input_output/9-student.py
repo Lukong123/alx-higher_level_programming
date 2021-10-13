65c2e33a1117b4b5413bf580914bd31acb984ca3
Anne Machinda <annemachinda@gmail.com>
2021-10-13 21:44:57 +0100

    stud

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,13 +5,13 @@ A module creates an object from a json file
 
 
 class Student:
-    """Class student"""
+    """Class student constructor"""
     def __init__(self, first_name, last_name, age):
-        """Constructor first_name, last_name, age"""
+        """Student object instance constructor"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """Retrieves dictionary"""
-            return (self.__dict__)
+    def to_json(self):
+        """retrieves a dictionary representation"""
+        return (self.__dict__)
