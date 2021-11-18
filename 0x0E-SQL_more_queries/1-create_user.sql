ffc79c58dd372590f959981095eed2c1b38854aa
Anne Machinda <annemachinda@gmail.com>
2021-11-18 03:31:33 +0100

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Script that create user and set priviledges
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
