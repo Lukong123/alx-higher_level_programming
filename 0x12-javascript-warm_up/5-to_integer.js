3b2dd26fb06fbfe6200a6e50a4b353766f4f0f2e
Anne Machinda <annemachinda@gmail.com>
2021-12-14 13:09:07 +0100

    multi language

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* converting to interger */
-const myNum = Number.parseInt (process.argv[2], 10);
+const myNum = Number.parseInt(process.argv[2], 10);
 if (isNaN(myNum)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log ( `My number: ${myNum}`);
+  console.log(`My number: ${myNum}`);
 }
