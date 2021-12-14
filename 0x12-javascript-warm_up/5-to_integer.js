b0161ded3e3cd157b91a6a200f9ef2d52b0679a7
Anne Machinda <annemachinda@gmail.com>
2021-12-14 12:51:23 +0100

    thrid

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// a script that prints My number: <first argument converted in integer>
-const num = Math.floor(Number(process.argv[2]));
-if (isNaN(num)) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${num}`);
-}
+const num = Number.parseInt(process.argv[2]);
+
+console.log(Number.isNaN(num) ? 'Not a number' : 'My number: ' + num);
