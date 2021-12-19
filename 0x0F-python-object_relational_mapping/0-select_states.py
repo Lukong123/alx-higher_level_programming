cdce421f4cbaee619187a3084c7d96a4fe424b86
Anne Machinda <annemachinda@gmail.com>
2021-12-19 22:51:38 +0100

    import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 """ Listing states in database"""
-
+import MySQLdb
 from sys import argv
-
+    
 
 if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-
     db_connection = MySQLdb.connect(user, passwd, db)
     cursor = db_connection.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
