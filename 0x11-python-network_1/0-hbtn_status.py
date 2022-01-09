be580b6d63449c0dbf09c19f33fb926550337277
Anne Machinda <annemachinda@gmail.com>
2022-01-09 22:32:46 +0100

    stats

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,12 @@ a Python script that fetches https://intranet.hbtn.io/status
 '''
 import urllib.request
 
+
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as rep:
-        html = rep.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        charset = html.decode('utf-8')
+        print("Body response:")
+        print(
+            "\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+            .format(type(html), html, charset))
