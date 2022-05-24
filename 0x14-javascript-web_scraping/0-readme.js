4f8998a63b64ac82d3f163045973cb10216a69b9
anne <annemachinda@gmail.com>
2022-05-24 13:03:20 +0100

    added parenthesis)

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints the content of a file
 const fs = require('fs');
-fs.readFile(process.argv[2]), 'utf-8', function(err, contents) {
+fs.readFile(process.argv[2], 'utf-8', function(err, contents) {
     if (err) {
 	return console.log(err);
     }
