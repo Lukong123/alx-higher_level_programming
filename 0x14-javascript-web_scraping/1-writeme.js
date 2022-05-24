776aa92023367ff520bbea49174f48e5915a2d8a
anne <annemachinda@gmail.com>
2022-05-24 13:11:12 +0100

    write

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// writes string to a file
+
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3],  'utf-8', function(err) {
+    if (err) {
+	return console.log(err);
+    }
+});
+
